bcc84a450bd184955bf3dd5a6db802dffaa39246
F1L1P <mlodzik.filip@wp.pl>
2023-01-02 11:31:44 +0100

    Dodanie zmiennych do przechowywania dat i długości podróży

--- app/src/main/java/com/example/kalendarzujto/MainActivity.kt
+++ app/src/main/java/com/example/kalendarzujto/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.example.kalendarzujto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    // Zmienne aby przechowywac daty
+    private var startDate: Calendar = Calendar.getInstance()
+    private var endDate: Calendar = Calendar.getInstance()
+
+    // Zmienne aby przechowywac ilosc dni
+    private var tripLength: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
